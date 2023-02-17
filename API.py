87e06e856f9f6664ecc533a7bab4195eb3e603a2
sravan parakala <sravanparakala@gmail.com>
2023-02-16 19:09:11 -0500

    Update API.py

--- API.py
+++ API.py
@@ -8,50 +8,50 @@ Created on Mon Feb  6 20:13:05 2023
 from flask import *
 import json
 import time
-
+def entry_time(user, l):
+    print("{} entered at {}:{} on {}/{}/{}".format(user, l[3],l[4], l[1],l[2], l[0]))
+    
+def exit_time(user, l):
+    print("{} left at {}:{} on {}/{}/{}".format(user, l[3],l[4], l[1],l[2], l[0]))
 app = Flask(__name__)
 
-user1 = {
-  "name": 'name1',
-  "isHere": True,
-  "hereAt": 0.0
 
-}
-user2 = {
-  "name": 'name2',
-  "isHere": True,
-  "hereAt": 0.0
-}
-user3 = {
-  "name": 'name3',
-  "isHere": True,
-  "hereAt": 0.0
-}
-users = [user1,user2,user3]
+users = []
 @app.route('/', methods=['GET'])
 def home_page():
-    
+    for user in users:
+        print(user['name'])
     return users
 
-@app.route('/user/', methods=['GET'])
-def request_page():
+@app.route('/add/', methods=['GET'])
+def add_page():
     
     
     user_query = str(request.args.get('user'))
-    
+    if user_query == '':
+        return json_dump
     for user in users:
         if user["name"] == user_query:
-            print(user_query, "has left")
-            user["isHere"] =False
-            json_dump = json.dumps(user)
-            users.remove(user)
-            return json_dump
+            print("Repeated user", user_query)
+            return json.dumps(user)
+    times = (time.time(),time.localtime())
     
-    data_set = {'name': user_query, 'isHere': True,  'Timestamp': time.time()}
+    data_set = {'name': user_query,'time': times[0]}
+    entry_time(user_query, times[1])
     users.append(data_set)
     json_dump = json.dumps(data_set)
     
     return json_dump
-    
+@app.route('/remove/', methods=['GET'])
+def remove_page():
+    user_query = str(request.args.get('user'))
+    if user_query == '':
+        return json.dumps()
+    for user in users:
+        if user["name"] == user_query:
+            users.remove(user)
+            return json.dumps(user)
+    print("User {} not in list", user_query)
+    return json.dumps(user_query)
 if __name__ == '__main__':
-    app.run(port = 7777)
+    app.run(host = "0.0.0.0")
